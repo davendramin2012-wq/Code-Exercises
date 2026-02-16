a42f2e2bb21d2790cb50d19acfdd087d6c5d23d8
Daniele Vendramin <davendramin2012@gmail.com>
2026-02-16 12:34:42 +0100

    Refactor Python script for clarity and consistency

--- Python/Esercizi_in_Python_1.py
+++ Python/Esercizi_in_Python_1.py
@@ -1,10 +1,13 @@
 print ("Tralalero Tralala")
 print (17*86)
+
 name = "Bill Gates"
 age = 70
 print ("Hi, the name of the Microsoft founder is", name , "and he is" , age ,"years old")
+
 birth_year = int(input("What year were you born? "))
 age = 2026 - birth_year
 print ("You are about", age, "years old.")
+
 
 input("Premi INVIO per chiudere...")
